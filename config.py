be92f6e1af59b57ad5b7fd88dade4f730ba1feab
jcmarkowicz <98119776+jcmarkowicz@users.noreply.github.com>
2026-07-13 21:49:46 -0400

    Update bankroll value from 2700 to 2000

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ from DataPipeline.FeatureEngineering.features_pipeline import BASE_DIR
 class config:
     
     # --- Bet Params ---
-    bankroll:int = 2700
+    bankroll:int = 2000
 
     # for opening and closing 
     mdd_ml = [.3, .4, .4] 
