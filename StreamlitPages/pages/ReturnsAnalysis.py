aee7d9d4d88d7081297790a0248dcb2ddc95bb5b
jcmark <jcmark@bu.edu>
2026-08-30 17:34:53 -0400

    fix

--- StreamlitPages/pages/ReturnsAnalysis.py
+++ StreamlitPages/pages/ReturnsAnalysis.py
@@ -17,10 +17,6 @@ show_image(
     title='Odds Returns: Moneyline and Parlay'
 )
 
-ml_results = pd.read_csv(config.ml_returns_fp)
-parlay_results = pd.read_csv(config.parlay_returns_fp)
-bankroll_results = pd.read_csv(config.bankroll_returns_fp)
-
 df_accuracy = pd.read_csv(settings.data_dir / 'betting_results' / 'prediction_accuracies.csv')
 df_bet_types = pd.read_csv(settings.data_dir / 'betting_results' / 'bet_types.csv')
 
