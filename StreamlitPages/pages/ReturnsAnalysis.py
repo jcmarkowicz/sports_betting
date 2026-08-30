448d88582ee34ef4975c431a18e477f8121e7895
jcmark <jcmark@bu.edu>
2026-08-29 20:58:32 -0400

    fixes

--- StreamlitPages/pages/ReturnsAnalysis.py
+++ StreamlitPages/pages/ReturnsAnalysis.py
@@ -1,33 +1,28 @@
 import streamlit as st
-
-import numpy as np 
 import pandas as pd 
-from collections import defaultdict
-
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 from StreamlitPages.utils import display_paginated_df, show_image
-from ufc_betting.config import config
-
-BASE_DIR = config.base_dir  
-
+from ufc_betting.config import config, settings
 
 
 # show_image(path, title='Percent Returns')
 st.title(" Prediction and Betting Results since 2026-02-21")
 
-
-show_image(path, title='Bankroll Returns: Moneyline + Parlay')
-
-show_image(path, title='Odds Returns: Moneyline and Parlay')
+show_image(
+    settings.data_dir / 'plot_pngs' / 'bankroll_returns_live.png'
+    , title='Bankroll Returns: Moneyline + Parlay'
+)
+show_image(
+    settings.data_dir / 'plot_pngs' / 'avg_returns_live.png',
+    title='Odds Returns: Moneyline and Parlay'
+)
 
 ml_results = pd.read_csv(config.ml_returns_fp)
 parlay_results = pd.read_csv(config.parlay_returns_fp)
 bankroll_results = pd.read_csv(config.bankroll_returns_fp)
 
-plot_returns(ml_results, parlay_results, bankroll_results)
+df_accuracy = pd.read_csv(settings.data_dir / 'betting_results' / 'prediction_accuracies.csv')
+df_bet_types = pd.read_csv(settings.data_dir / 'betting_results' / 'bet_types.csv')
 
-df_accuracy, df_bet_types = accuracy_analysis(ml_results, parlay_results)
 display_paginated_df(df_accuracy, title='Returns Accuracy', key_prefix='returns')
 display_paginated_df(df_bet_types, title='Bet Type Stats', key_prefix='')
